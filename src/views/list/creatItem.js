54364bc3129e8a40589174ea8e1fed229531c95b
李月茹 <liyueru@treefinance.com.cn>
2019-05-16 17:48:13 +0800

    折扣

--- src/views/list/creatItem.js
+++ src/views/list/creatItem.js
@@ -1,7 +1,23 @@
 import Item from './item';
 
 // 补充：优惠商品的处理逻辑
+function createDiscount(itemData) {
+	// 用代理做折扣
+	return new Proxy(itemData, {
+		get(target, key) {
+			if (key === 'name') {
+				return `${target[key]} [折扣]`;
+			}
+			if (key === 'price') {
+				return `原价：${target[key]}, 折后：${target[key] * 0.8}`;
+			}
+		},
+	});
+}
 // 工厂函数
 export default function (list, itemData) {
+	if (itemData.discount) {
+		itemData = createDiscount(itemData);
+	}
 	return new Item(list, itemData);
 }
--- src/views/shoppingCart/index.js
+++ src/views/shoppingCart/index.js
@@ -1,10 +1,34 @@
 import $ from 'jquery';
+import getCart from './getCart';
 
 export default class ShoppingCart {
 	constructor(app) {
 		this.app = app;
-		this.$el = $('<div></div>');
+		this.$el = $('<div></div>').css({
+			padding: '10px',
+			border: '1px solid #ccc',
+		});
+		this.cart = getCart();
 	}
 
-	init() {}
+	initBtn() {
+		const $btn = $('<button>购物车</button>');
+		$btn.click(() => {
+			this.showCart();
+		});
+		this.$el.append($btn);
+	}
+
+	showCart() {
+		alert(this.cart.getList());
+	}
+
+	render() {
+		this.app.$el.append(this.$el);
+	}
+
+	init() {
+		this.initBtn();
+		this.render();
+	}
 }
